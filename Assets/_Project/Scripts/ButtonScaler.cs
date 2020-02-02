94d57c6a0dad9145e10b65a308d52f7b325b812a
Ole Midthun <olemidthun@Oles-MacBook-Pro.local>
2020-02-02 14:20:23 +0100

    Add final art assets

---
+++ Assets/_Project/Scripts/ButtonScaler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ButtonScaler : MonoBehaviour
+{
+
+    public float maxScale = 1.5f;
+    public float speed = 0.25f;
+
+    void Update()
+    {
+        var t = Mathf.PingPong(Time.time * 2 * speed, 1);
+
+        var size = Mathf.Lerp(1, maxScale, t);
+
+        GetComponent<RectTransform>().localScale = new Vector3(size, size, size);
+    }
+}
